3b1949385a396f4448ca41f7f4bf9f9ac7590bbb
진연형/Cloud운영그룹(무선)/Staff Engineer/삼성전자 <CORP\smiler.jin@U18PB1-0046.tn.corp.samsungelectronics.net>
2021-03-26 13:47:15 +0900

    add interrupt token keys on metadata
    Signed-off-by: 진연형/Cloud운영그룹(무선)/Staff Engineer/삼성전자 <CORP\smiler.jin@U18PB1-0046.tn.corp.samsungelectronics.net>

--- src/spaceone/spot_automation/model/plugin_response_model.py
+++ src/spaceone/spot_automation/model/plugin_response_model.py
@@ -10,6 +10,8 @@ _SUPPORTED_RESOURCE_TYPE = [
     'inventory.CloudService?provider=aws&cloud_service_group=EC2&cloud_service_type=AutoScalingGroup'
 ]
 
+_INTERRUPT_TOKEN_KEYS = ['data.account_id', 'region_code']
+
 _TEMPLATE = {
     'StartAt': 'getAnyUnprotectedOndemandInstance',
     'States': {
@@ -66,6 +68,7 @@ _TEMPLATE = {
 
 class PluginMetadata(Model):
     supported_resource_type = ListType(StringType, default=_SUPPORTED_RESOURCE_TYPE)
+    interrupt_token_keys = ListType(StringType, default=_INTERRUPT_TOKEN_KEYS)
     template_json = json.dumps(_TEMPLATE)
     template = StringType(required=True, default=template_json)
 
